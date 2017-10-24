c6acd34489c84e0c1fb430d8d3062802fb702436
gorZ <lemaitc.greg@gmail.com>
2017-10-24 19:35:36 +0800

    Added notification

--- app/src/main/java/com/android/bentabasura/benta_basura/Fragments/BuyCrafted_TabFragmentItemDetails.java
+++ app/src/main/java/com/android/bentabasura/benta_basura/Fragments/BuyCrafted_TabFragmentItemDetails.java
@@ -8,6 +8,7 @@ import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -18,6 +19,7 @@ import android.widget.Toast;
 
 import com.android.bentabasura.benta_basura.Models.ActiveUser;
 import com.android.bentabasura.benta_basura.Models.Craft;
+import com.android.bentabasura.benta_basura.Models.Notification;
 import com.android.bentabasura.benta_basura.Pages.Login;
 import com.android.bentabasura.benta_basura.Pages.MyItems_Edit_Craft;
 import com.android.bentabasura.benta_basura.R;
@@ -29,7 +31,10 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 import com.squareup.picasso.Picasso;
 
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
 
 public class BuyCrafted_TabFragmentItemDetails extends Fragment implements View.OnClickListener{
 
@@ -41,7 +46,8 @@ public class BuyCrafted_TabFragmentItemDetails extends Fragment implements View.
     ImageView imgThumbCraft;
     Bundle receivedBundle;
     Intent receiveIntent,editCraftpage,detailsIntent,sellerdetailsIntent;
-    DatabaseReference databaseReference;
+    DatabaseReference databaseReference, databaseReferenceNotif;
+    Boolean found = false;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -108,13 +114,14 @@ public class BuyCrafted_TabFragmentItemDetails extends Fragment implements View.
                 detailsIntent = new Intent(getActivity().getApplicationContext(), MyItems_Edit_Craft.class);
                 detailsIntent.putExtra("CraftID", craftid);
                 detailsIntent.putExtra("CraftCategory", craftCategory);
-
                 startActivity(detailsIntent);
                 break;
 
             case R.id.btnInterested:
                 sellerdetailsIntent = new Intent(getActivity().getApplicationContext(),custom_dialog_disclaimer.class);
                 sellerdetailsIntent.putExtra("UserId",receivedBundle.get("UploadedBy").toString());
+
+                addNotification();
                 startActivity(sellerdetailsIntent);
                 break;
         }
@@ -122,5 +129,64 @@ public class BuyCrafted_TabFragmentItemDetails extends Fragment implements View.
 
     public void showMessage(String message){
         Toast.makeText(getActivity().getApplicationContext(), message , Toast.LENGTH_LONG).show();
+    }
+
+    public void addNotification()
+    {
+        final String profileImage = activeUser.getProfilePicture();
+        final String profileName = activeUser.getFullname();
+
+        Date currentTime = Calendar.getInstance().getTime();
+        SimpleDateFormat sdf = new SimpleDateFormat("E MMM dd yyyy hh:mm a");
+        final String UploadedDate = sdf.format(currentTime);
+
+        //Notification
+        final String notifId = databaseReference.child("Notification").push().getKey();
+        final String location = "Craft" + ":" + receivedBundle.get("CraftCategory").toString() + ":" + receivedBundle.get("CraftId").toString();
+        final String message = activeUser.getFullname() + " is interested in your Craft " + receivedBundle.get("CraftName").toString() + ". Expect a call from him/her";
+        final String ownerId = receivedBundle.get("UploadedBy").toString();
+        final String profileId = activeUser.getUserId();
+
+        if (!TextUtils.isEmpty(location) && !TextUtils.isEmpty(message) && !TextUtils.isEmpty(ownerId) && !TextUtils.isEmpty(profileId) && !TextUtils.isEmpty(profileImage) && !TextUtils.isEmpty(UploadedDate) )
+        {
+            final Notification newNotif = new Notification();
+
+            newNotif.setNotifDbLink(location);
+            newNotif.setNotifMessage(message);
+            newNotif.setNotifOwnerId(ownerId);
+            newNotif.setNotifBy(profileId);
+            newNotif.setNotifRead("0");
+            newNotif.setNotifNotify("0");
+            newNotif.setNotifByPic(profileImage);
+            newNotif.setNotifDate(UploadedDate);
+
+
+            databaseReference.child("Craft").child(receivedBundle.get("CraftCategory").toString()).child(receivedBundle.get("CraftId").toString()).child("Interested").addListenerForSingleValueEvent(new ValueEventListener() {
+                @Override
+                public void onDataChange(DataSnapshot dataSnapshot) {
+                    found = false;
+                    for(DataSnapshot postSnapShot:dataSnapshot.getChildren())
+                    {
+                        if (postSnapShot.getValue().toString().equals(profileId))
+                        {
+                            found = true;
+                            break;
+                        }
+
+                    }
+
+                    if(!found)
+                    {
+                        databaseReference.child("Notification").child(notifId).setValue(newNotif);
+                        databaseReference.child("Craft").child(receivedBundle.get("CraftCategory").toString()).child(receivedBundle.get("CraftId").toString()).child("Interested").push().setValue(profileId);
+                    }
+                }
+
+                @Override
+                public void onCancelled(DatabaseError databaseError) {
+
+                }
+            });
+        }
     }
 }
