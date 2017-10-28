01f1c05a1df0c6110613f8ea0ee2bcc39795b61a
ccs <ccs@ccss-MacBook-Air.local>
2017-10-28 12:51:28 +0800

    Add permission in appintro

--- app/src/main/java/com/android/bentabasura/benta_basura/Page_Adapters/MyIntroPageAdapter.java
+++ app/src/main/java/com/android/bentabasura/benta_basura/Page_Adapters/MyIntroPageAdapter.java
@@ -1,7 +1,10 @@
 package com.android.bentabasura.benta_basura.Page_Adapters;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
 import android.widget.Toast;
 
 import com.android.bentabasura.benta_basura.Pages.Login;
@@ -16,6 +19,11 @@ public class MyIntroPageAdapter extends AppIntro {
     // Please DO NOT override onCreate. Use init
     @Override
     public void init(Bundle savedInstanceState) {
+
+        ActivityCompat.requestPermissions(MyIntroPageAdapter.this,
+                new String[]{Manifest.permission.INTERNET, Manifest.permission.ACCESS_WIFI_STATE, Manifest.permission.ACCESS_NETWORK_STATE, Manifest.permission.READ_EXTERNAL_STORAGE, Manifest.permission.WRITE_EXTERNAL_STORAGE,
+                        Manifest.permission.CAMERA,Manifest.permission.CALL_PHONE, Manifest.permission.VIBRATE },
+                1);
 
         //adding the three slides for introduction app you can ad as many you needed
         addSlide(AppIntroSlider.newInstance(R.layout.app_intro_first));
@@ -58,5 +66,31 @@ public class MyIntroPageAdapter extends AppIntro {
     @Override
     public void onSlideChanged() {
         // Do something here when slide is changed
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case 1: {
+
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+                    // permission was granted, yay! Do the
+                    // contacts-related task you need to do.
+                } else {
+
+                    // permission denied, boo! Disable the
+                    // functionality that depends on this permission.
+                    Toast.makeText(MyIntroPageAdapter.this, "Permission denied", Toast.LENGTH_SHORT).show();
+                }
+                return;
+            }
+
+            // other 'case' lines to check for other
+            // permissions this app might request
+        }
     }
 }
